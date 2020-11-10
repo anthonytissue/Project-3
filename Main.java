6538ec36ea2ab89c494614c0e3bc2eb14fe3a44f
anthonytissue <anthony.tissue@patelhs.org>
2020-11-10 17:25:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Ms. Bennys class, please find your seat.");
+    int[] seatNumber = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    String[] names = new String[15];
+    Scanner scan = new Scanner(System.in);
+
+    for (int i = 0; i < 15; i++) {
+    System.out.println("What is your name?");
+    names[i] = scan.next();
+    }
+    int i = 0;
+    while (i < 15) {
+      System.out.println (names[i] + " Goes to their chair");
+      i++;
+    }
+
+
+  }
+}
